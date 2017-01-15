bc6ab984826f5a17ad78ea268923b4deb2123528
Vitali Dettling <vitali.dettling@gmail.com>
2017-01-15 13:40:08 +0100

    patmat: wordOccurrences && sentenceOccurrences

--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -76,10 +76,15 @@ class HuffmanSuite extends FunSuite {
     }
   }
 
+  test("decode and encode a very short text should be identity") {
+    new TestTrees {
+      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
 
-//  test("decode and encode a very short text should be identity") {
-//    new TestTrees {
-//      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
-//    }
-//  }
+  test("2 decode and encode a very short text should be identity") {
+    new TestTrees {
+      assert(decode(t1, encode(t1)("ba".toList)) === "ba".toList)
+    }
+  }
 }
